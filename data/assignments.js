4c52789e8ff8ca961e828f9f17742f8b8968064b
Nick Lavery <tuneboynick@gmail.com>
2020-07-22 19:45:48 -0400

    See answer two: import and export

---
+++ data/assignments.js
@@ -0,0 +1,18 @@
+let assignments = [{
+    assignment1: {
+        name: '',
+        completed: true
+    },
+    assignment2: {
+        name: '',
+        completed: false
+    },
+    assignment3: {
+        name: '',
+        completed: true
+    },
+}];
+
+module.exports = {
+    assignments
+}
--- index.js
+++ index.js
@@ -9,6 +9,7 @@
     1)  Declare and initialize an array of `assignments` with `name` and `completed` properties. Name the array `assignments`.  `completed` will have boolean values.
 
  */
+/*
 let assignments = [{
     assignment1: {
         name: '', 
@@ -23,24 +24,31 @@ let assignments = [{
         completed: true
     },
 }];
+*/
 
- /**
 
-    Referencing code in other files
- 
-    2a)  Great work! Now move that array initialization out into its own file. 
-        See ./data/assignments.js where you will initialize the data in place of inline in this function
 
-    2b) "Import" that data into this file in place of the inline code you had in step 1
-    
+// Referencing code in other files
 
-  */
+/* 2a)  Great work! Now move that array initialization out into its own file. 
+See ./data/assignments.js where you will initialize the data in place of inline in this function
+copy/pasted the function into assignments.js and added `module.exports = {
+assignments
+}`
+*/
+
+// 2b) "Import" that data into this file in place of the inline code you had in step 1
+const { assignments } = require('./data/assignments');
+
+console.log(assignments);
+
+
 
 
 /**
 
     Looping and using references to arrays a given positions
- 
+
     3) Loop through the data using a for loop. Just console.log within the loop to show each item within the array
 
  */
@@ -48,7 +56,7 @@ let assignments = [{
 /**
 
     Looping and Assignment
- 
+
     4) Declare a new array named `allAssignments`. Loop through the `assignments` array data using a for loop and assign each item from `assignments` to the new `allAssignments` array
 
  */
@@ -57,7 +65,7 @@ let assignments = [{
 /**
 
     Filtering
- 
+
     5) Declare a new array named `completedAssignments`. Loop through the `assignments` array data using a for loop.  Use a condition to only add to `allAssignments` where there are property values with `completed` of `true`
 
  */
