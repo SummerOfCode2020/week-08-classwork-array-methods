250bb909dfb6baa887e45520fc52969fc2283864
Yor Name <hollisjamison@gmail.com>
2020-07-22 21:29:06 -0400

    Added new exampeles to verify it works

--- data/assignments.js
+++ data/assignments.js
@@ -5,7 +5,15 @@ let assignments = [{
     {
         name: 'Week 9 Homework',
         completed: false
-    }
+    },
+        {
+            name: 'Week 10 Homework',
+            completed: false
+        },
+            {
+                name: 'Week 11 Homework',
+                completed: true
+            }
 ]
 
 module.exports = {assignments}
