dde086563af9d34b3ef0744bada152ae8327227c
mvrcusj <mvrcusj@gmail.com>
2020-07-22 21:29:48 -0400

    Declare a new array named completedAssignments and push the completed assignments from the original array to the new array

--- index.js
+++ index.js
@@ -13,6 +13,9 @@
     }, {
         name: 'project 2',
         completed: false
+    }, {
+        name:'project 3',
+        completed: true
     }]
 
     console.log(assignments)
@@ -26,20 +29,74 @@
 
 // using for-in with an array gives us access to the index number value only
 
+ /**
 
-
-    Looping and Assignment
+    Referencing code in other files
  
-    4) Declare a new array named `allAssignments`. Loop through the `assignments` array data using a for loop and assign each item from `assignments` to the new `allAssignments` array
+    2a)  Great work! Now move that array initialization out into its own file. 
+        See ./data/assignments.js where you will initialize the data in place of inline in this function
 
- */  
+    2b) "Import" that data into this file in place of the inline code you had in step 1
+    
+
+  */
+
+
+/**
+
+    Looping and using references to arrays a given positions
+ 
+    3) Loop through the data using a for loop. Just console.log within the loop to show each item within the array
+
+ */
+// option 1:
+for (let index = 0; index < assignments.length; index++) {
+    // singular variable created to make "assignments[index]" easy to type in the future as just "assignment" to reference it
+    const assignment = assignments[index]
+    console.log({ assignment })
+}
+
+
+// option 2:
+// using a for-in with an array gives us access to the index number value only
+for (let index in assignments) {
+    const assignment = assignments[index]
+    console.log(index, assignment)
+}
+
+// option 3:
+assignments.forEach( function (assignment) {
+    console.log({ assignment })
+} )
+//or 
+assignments.forEach( (assignment) => {
+    console.log({ assignment })
+} )
+
+//    Looping and Assignment
+
+//    4) Declare a new array named `allAssignments`. Loop through the `assignments` array data using a for loop and assign each item from `assignments` to the new `allAssignments` array
+
+
 let allAssignments = []
 
 assignments.forEach( (assignment) => {
     allAssignments.push(assignment)
 })
 
-console.log({ allAssignments})
+console.log({ allAssignments })
+
+// map similar to forEach
+
+//let allAssignment = assignments.map( (assignment) => {
+//    return assignment
+//} ) 
+
+// or to remove excess syntax
+
+//let allAssignments = assignments.map( assignment => assignment )
+
+//console.log(allAssignments)
 
 
 
@@ -51,8 +108,16 @@ console.log({ allAssignments})
 
     Filtering
  
-    5) Declare a new array named `completedAssignments`. Loop through the `assignments` array data using a for loop.  Use a condition to only add to `allAssignments` where there are property values with `completed` of `true`
+    5) Declare a new array named `completedAssignments`. Loop through the `assignments` array data using a for loop.  Use a condition to only add to `completedAssignments` where there are property values with `completed` of `true`
 
  */
 
+let completedAssignments = []
 
+assignments.forEach( (assignment) => {
+    if (assignment.completed === true) {
+        return completedAssignments.push(assignment)
+    }
+}) 
+
+console.log({ completedAssignments })
